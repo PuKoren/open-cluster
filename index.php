f95c3b80fb60bc66e1780b576ca708bf53130fd0
PuKoren <koren@pukogames.com>
2013-08-06 15:49:17 +0200

    First commit, adding base files for website, full static atm

---
+++ index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>This is a game</title>
+		<link rel="stylesheet" type="text/css" href="css/reset-min.css"></link>
+		<link rel="stylesheet" type="text/css" href="css/default.css"></link>
+		<script type="text/javascript" src="http://codeorigin.jquery.com/jquery-2.0.3.min.js"></script>
+	</head>
+	<body>
+		<div class="menu">
+			<div class="menuItems defaultAlign">
+				<ul>
+					<li>
+						<a href=".">This is a Game</a>
+					</li>
+					<li>
+						<a class="selected" href="">Home</a>
+					</li>
+					<li>
+						<a class="" href="">How to Play</a>
+					</li>
+					<li>
+						<a class="" href="">Leaderboard</a>
+					</li>
+					<li>
+						<a class="" href="">Forums</a>
+					</li>
+					<li>
+						<a class="connect" href="">Log In</a>
+					</li>
+				</ul>
+			</div>
+		</div>
+		<div class="title defaultAlign">
+			<span id="date" class="left">August, 6th</span><span id="time" class="right">11:46</span>
+		</div>
+		<div class="container defaultAlign">
+			<img class="header" src="resources/img/planet.png"></img>
+			<div class="content">
+				<img class="right" src="resources/img/spaceship.jpg"/>
+				<h1>I can do that</h1>
+
+				<p>Now that there is the Tec-9, a crappy spray gun from South Miami. This gun is advertised as the most popular gun in American crime. Do you believe that shit? It actually says that in the little book that comes with it: the most popular gun in American crime. Like they're actually proud of that shit.
+				<br/>My money's in that office, right? If she start giving me some bullshit about it ain't there, and we got to go someplace else and get it, I'm gonna shoot you in the head then and there. Then I'm gonna shoot that bitch in the kneecaps, find out where my goddamn money is. She gonna tell me too. Hey, look at me when I'm talking to you, motherfucker. You listen: we go in there, and that nigga Winston or anybody else is in there, you the first motherfucker to get shot. You understand? </p>
+
+				<h1>Is she dead, yes or no?</h1>
+				<p>My money's in that office, right? If she start giving me some bullshit about it ain't there, and we got to go someplace else and get it, I'm gonna shoot you in the head then and there. Then I'm gonna shoot that bitch in the kneecaps, find out where my goddamn money is. She gonna tell me too. Hey, look at me when I'm talking to you, motherfucker. You listen: we go in there, and that nigga Winston or anybody else is in there, you the first motherfucker to get shot. You understand? </p>
+			</div>
+		</div>
+		<div class="footer">
+			No copyright 2013
+		</div>
+		<script type="text/javascript" src="js/default.js"></script>
+	</body>
+</html>
