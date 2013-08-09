9e5c2c837aaecde18d732131cb75bd173745006b
PuKoren <koren@pukogames.com>
2013-08-09 12:12:05 +0200

    Removed unused file (user login)

--- application/views/user/loginsuccess.php
+++
@@ -1,7 +0,0 @@
-<img class="header" src="<?php echo site_url('resources/img/banners/planet.png');?>"></img>
-<div class="content">
-	<h1>Login</h1>
-	<p>
-		Everything is allright Sir ! Please proceed to the <a href="<?php echo site_url('play')?>">command room</a>.
-	</p>
-</div>
